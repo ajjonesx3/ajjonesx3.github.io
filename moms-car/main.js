db174f7edf7ed379cc1515efc825172b7c98965d
ajjonesx3 <ajjonesx3@gmail.com>
2024-05-11 15:16:10 -0400

    Ay bruh hope this works

--- moms-car/main.js
+++ moms-car/main.js
@@ -5,10 +5,25 @@ function populate() {
 		.then((data) => {
 			let da = data.array;
 			for(let i=0;i<da.length;i++){
-				let left = da[i][0];
-				let right = da[i][1];
-				console.log(left);
-				//make list items out of left and right
+				let cur_list = da[i][0];
+				let entries = da[i][1];
+				for(let n=0;n<entries.length;n++){
+					let cur_entry = entries[n];
+					let left = cur_entry[0];
+					let right = cur_entry[1];
+					let left_span = document.createElement('span');
+					left_span.classList.add("left");
+					let right_span = document.createElement('span');
+					right_span.classList.add("right");
+					let cur_list = document.getElementById(cur_list).value;
+					let li = document.createElement('li');
+					let dive = document.createElement('div');
+					dive.classList.add("entry");
+					dive.appendChild(left_span);
+					dive.appendChild(right_span);
+					li.appendChild(dive);
+					cur_list.appendChild(li);
+				}
 			}
 		}).catch(err => console.error(err));
 
