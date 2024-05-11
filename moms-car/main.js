9ba2a61a67ba5922a7b3a16f63a7d1dfd24dbcc7
ajjonesx3 <ajjonesx3@gmail.com>
2024-05-11 15:57:11 -0400

    Added text to left and right spans

--- moms-car/main.js
+++ moms-car/main.js
@@ -6,15 +6,17 @@ function populate() {
 			let da = data.array;
 			for(let i=0;i<da.length;i++){
 				var cur_list = da[i][0];
-				let entries = da[i][1];
+				var entries = da[i][1];
 				for(let n=0;n<entries.length;n++){
 					let cur_entry = entries[n];
 					let left = cur_entry[0];
 					let right = cur_entry[1];
 					let left_span = document.createElement('span');
 					left_span.classList.add("left");
+					left_span.textContent = left;
 					let right_span = document.createElement('span');
 					right_span.classList.add("right");
+					right_span.textContent = right;
 					let cur_ol = document.getElementById(cur_list);
 					let li = document.createElement('li');
 					let dive = document.createElement('div');
